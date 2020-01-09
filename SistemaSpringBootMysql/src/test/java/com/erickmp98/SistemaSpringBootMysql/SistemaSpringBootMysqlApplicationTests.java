5d57e6ca6a045c7cd5381f35f07c295c75ee3b86
Erick Alexis Molleapaza Palomino <molleapaza98@gmail.com>
2020-01-09 11:51:46 -0500

    Commit Inicial con STS

---
+++ SistemaSpringBootMysql/src/test/java/com/erickmp98/SistemaSpringBootMysql/SistemaSpringBootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmp98.SistemaSpringBootMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSpringBootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
